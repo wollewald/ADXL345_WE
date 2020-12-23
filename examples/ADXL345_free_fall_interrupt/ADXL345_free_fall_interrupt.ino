51c4314e278765350268012b0a6cfc260afffc40
Wolfgang (Wolle) Ewald <wolfgang.ewald@wolles-elektronikkiste.de>
2020-12-23 16:10:29 +0100

    Add files via upload

---
+++ examples/ADXL345_free_fall_interrupt/ADXL345_free_fall_interrupt.ino
@@ -0,0 +1,134 @@
+/***************************************************************************
+* Example sketch for the ADXL345_WE library
+*
+* This sketch shows how to use the free fall interrupt.
+*   
+* Further information can be found on: 
+* http://wolles-elektronikkiste.de
+* 
+***************************************************************************/
+
+#include<Wire.h>
+#include<ADXL345_WE.h>
+#define ADXL345_I2CADDR 0x53
+const int int2Pin = 2;
+volatile bool freeFall = false;
+
+ADXL345_WE myAcc(ADXL345_I2CADDR);
+
+void setup(){
+  Wire.begin();
+  Serial.begin(9600);
+  pinMode(int2Pin, INPUT);
+  Serial.println("ADXL345_Sketch - Free Fall Interrupt");
+  Serial.println();
+  if(!myAcc.init()){
+    Serial.println("ADXL345 not connected!");
+  }
+
+/* Insert your data from ADXL345_calibration.ino and uncomment for more precise results */
+  // myAcc.setCorrFactors(-266.0, 285.0, -268.0, 278.0, -291.0, 214.0);
+
+/* Choose the data rate         Hz
+    ADXL345_DATA_RATE_3200    3200
+    ADXL345_DATA_RATE_1600    1600
+    ADXL345_DATA_RATE_800      800
+    ADXL345_DATA_RATE_400      400
+    ADXL345_DATA_RATE_200      200
+    ADXL345_DATA_RATE_100      100
+    ADXL345_DATA_RATE_50        50
+    ADXL345_DATA_RATE_25        25
+    ADXL345_DATA_RATE_12_5      12.5  
+    ADXL345_DATA_RATE_6_25       6.25
+    ADXL345_DATA_RATE_3_13       3.13
+    ADXL345_DATA_RATE_1_56       1.56
+    ADXL345_DATA_RATE_0_78       0.78
+    ADXL345_DATA_RATE_0_39       0.39
+    ADXL345_DATA_RATE_0_20       0.20
+    ADXL345_DATA_RATE_0_10       0.10
+*/
+  myAcc.setDataRate(ADXL345_DATA_RATE_25);
+  Serial.print("Data rate: ");
+  Serial.print(myAcc.getDataRateAsString());
+ 
+/* Choose the measurement range
+    ADXL345_RANGE_16G    16g     
+    ADXL345_RANGE_8G      8g     
+    ADXL345_RANGE_4G      4g   
+    ADXL345_RANGE_2G      2g
+*/  
+  myAcc.setRange(ADXL345_RANGE_2G);
+  Serial.print("  /  g-Range: ");
+  Serial.println(myAcc.getRangeAsString());
+  Serial.println();
+
+/* The parameters of the setFreeFallThresholds function are:  
+     - g threshold - do not choose a parameter which is too low. 0.3 - 0.6 g is fine.
+     - time threshold in ms, maximum is 1275. Recommended is 100 - 350;
+    If time threshold is too low, vibrations can be detected as free fall. 
+ */
+  myAcc.setFreeFallThresholds(0.4, 100);
+  
+/* You can choose the following interrupts:
+     Variable name:             Triggered, if:
+    ADXL345_OVERRUN      -   new data replaces unread data
+    ADXL345_WATERMARK    -   the number samples in FIFO equals number defined in FIFO_CTL
+    ADXL345_FREEFALL     -   acceleration values of all axes are below the thresold defined in THRESH_FF 
+    ADXL345_INACTIVITY   -   acc. value of all included axes are < THRESH_INACT for period > TIME_INACT
+    ADXL345_ACTIVITY     -   acc. value of included axes are > THRESH_ACT
+    ADXL345_DOUBLE_TAP   -   double tap detected on one incl. axis and various defined conditions are met
+    ADXL345_SINGLE_TAP   -   single tap detected on one incl. axis and various defined conditions are met
+    ADXL345_DATA_READY   -   new data available
+
+    Assign the interrupts to INT1 (INT_PIN_2) or INT2 (INT_PIN_2). Data ready, watermark and overrun are 
+    always enabled. You can only change the assignment of these which is INT1 by default.
+
+    You can delete interrupts with deleteInterrupt(type);
+*/
+  myAcc.setInterrupt(ADXL345_FREEFALL, 2);
+  
+  attachInterrupt(digitalPinToInterrupt(int2Pin), freeFallISR, RISING); 
+  freeFall=false; 
+}
+
+void loop() {
+  xyzFloat raw = myAcc.getRawValues();
+  xyzFloat g = myAcc.getGValues();
+     
+  Serial.print("Raw-x = ");
+  Serial.print(raw.x);
+  Serial.print("  |  Raw-y = ");
+  Serial.print(raw.y);
+  Serial.print("  |  Raw-z = ");
+  Serial.println(raw.z);
+
+  Serial.print("g-x   = ");
+  Serial.print(g.x);
+  Serial.print("  |  g-y   = ");
+  Serial.print(g.y);
+  Serial.print("  |  g-z   = ");
+  Serial.println(g.z);
+
+  Serial.println();
+ 
+  if(freeFall==true){
+    Serial.println("Aaaaaaaaah!!!!! - I'm faaaaallllling!");
+    delay(1000);
+    freeFall = false;
+    /* by reading the interrupt register the interrupt is cleared */
+    myAcc.readAndClearInterrupts();
+    
+    /* if you expect also other interrupts you can check the type. For this comment the previous line, 
+    and replace by the following four lines: */
+    //byte intType = myAcc.readAndClearInterrupts();
+    //if(myAcc.checkInterrupt(intType, ADXL345_FREEFALL)){
+    //  Serial.println("FREEFALL confirmed");
+    //}
+  }
+
+  delay(500);
+}
+
+void freeFallISR(){
+  freeFall = true;
+}
