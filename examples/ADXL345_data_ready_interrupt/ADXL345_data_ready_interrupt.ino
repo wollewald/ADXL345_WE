20edd31b66af067c241d71a24a57215db8add653
Wolfgang (Wolle) Ewald <wolfgang.ewald@wolles-elektronikkiste.de>
2020-12-23 16:07:00 +0100

    Add files via upload

---
+++ examples/ADXL345_data_ready_interrupt/ADXL345_data_ready_interrupt.ino
@@ -0,0 +1,115 @@
+/***************************************************************************
+* Example sketch for the ADXL345_WE library
+*
+* This sketch shows how to use the data ready interrupt.
+*   
+* Further information can be found on: 
+* http://wolles-elektronikkiste.de
+* 
+***************************************************************************/
+
+#include<Wire.h>
+#include<ADXL345_WE.h>
+#define ADXL345_I2CADDR 0x53
+const int int2Pin = 2;  // interrupt pin
+volatile bool dataReady = false;
+
+ADXL345_WE myAcc(ADXL345_I2CADDR);
+
+void setup(){
+  Wire.begin();
+  Serial.begin(9600);
+  pinMode(int2Pin, INPUT);
+  Serial.println("ADXL345_Sketch - Data Ready Interrupt");
+  Serial.println();
+  if(!myAcc.init()){
+    Serial.println("ADXL345 not connected!");
+  }
+
+/* Insert your data from ADXL345_calibration.ino and uncomment for more precise results */
+  // myAcc.setCorrFactors(-266.0, 285.0, -268.0, 278.0, -291.0, 214.0);
+
+/* Choose the data rate         Hz
+    ADXL345_DATA_RATE_3200    3200
+    ADXL345_DATA_RATE_1600    1600
+    ADXL345_DATA_RATE_800      800
+    ADXL345_DATA_RATE_400      400
+    ADXL345_DATA_RATE_200      200
+    ADXL345_DATA_RATE_100      100
+    ADXL345_DATA_RATE_50        50
+    ADXL345_DATA_RATE_25        25
+    ADXL345_DATA_RATE_12_5      12.5  
+    ADXL345_DATA_RATE_6_25       6.25
+    ADXL345_DATA_RATE_3_13       3.13
+    ADXL345_DATA_RATE_1_56       1.56
+    ADXL345_DATA_RATE_0_78       0.78
+    ADXL345_DATA_RATE_0_39       0.39
+    ADXL345_DATA_RATE_0_20       0.20
+    ADXL345_DATA_RATE_0_10       0.10
+*/
+// Choose a low frequency to showcase the data rate controlled output
+  myAcc.setDataRate(ADXL345_DATA_RATE_0_20);
+  Serial.print("Data rate: ");
+  Serial.print(myAcc.getDataRateAsString());
+
+/* Choose the measurement range
+    ADXL345_RANGE_16G    16g     
+    ADXL345_RANGE_8G      8g     
+    ADXL345_RANGE_4G      4g   
+    ADXL345_RANGE_2G      2g
+*/
+  myAcc.setRange(ADXL345_RANGE_2G);
+  Serial.print("  /  g-Range: ");
+  Serial.println(myAcc.getRangeAsString());
+  Serial.println();
+  
+  attachInterrupt(digitalPinToInterrupt(int2Pin), dataReadyISR, RISING);
+
+/* Default Interrupt Pin Polarity is active high. Change if you like */  
+  // myAcc.setInterruptPolarity(ADXL345_ACT_LOW);
+
+/* You can choose the following interrupts:
+     Variable name:             Triggered, if:
+    ADXL345_OVERRUN      -   new data replaces unread data
+    ADXL345_WATERMARK    -   the number samples in FIFO equals number defined in FIFO_CTL
+    ADXL345_FREEFALL     -   acceleration values of all axes are below the thresold defined in THRESH_FF 
+    ADXL345_INACTIVITY   -   acc. value of all included axes are < THRESH_INACT for period > TIME_INACT
+    ADXL345_ACTIVITY     -   acc. value of included axes are > THRESH_ACT
+    ADXL345_DOUBLE_TAP   -   double tap detected on one incl. axis and various defined conditions are met
+    ADXL345_SINGLE_TAP   -   single tap detected on one incl. axis and various defined conditions are met
+    ADXL345_DATA_READY   -   new data available
+
+    Assign the interrupts to INT1 (INT_PIN_2) or INT2 (INT_PIN_2). Data ready, watermark and overrun are 
+    always enabled. You can only change the assignment of these which is INT1 by default.
+
+    You can delete interrupts with deleteInterrupt(type);
+*/
+  myAcc.setInterrupt(ADXL345_DATA_READY, INT_PIN_2); 
+}
+
+/* In the following loop there is only one interrupt type that can occur on INT2.
+    In cases where you expect more candidates, you can check as follows:
+        uint8_t intType = myAcc.readAndClearInterrupts();
+        if(myAcc.checkInterrupt(intType, ADXL345_DATA_READY)){
+          Serial.println("DATA READY confirmed");
+        }
+*/
+void loop() {
+  // you see here is no delay to control the output rate
+  if(dataReady==true){
+    xyzFloat g = myAcc.getGValues();
+      
+    Serial.print("g-x   = ");
+    Serial.print(g.x);
+    Serial.print("  |  g-y   = ");
+    Serial.print(g.y);
+    Serial.print("  |  g-z   = ");
+    Serial.println(g.z);
+  
+    dataReady = false;
+  }  
+}
+
+void dataReadyISR(){
+  dataReady = true;
+}
