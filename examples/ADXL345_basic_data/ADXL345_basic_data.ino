231ab21cb3fd082d8ca06bfc506c2baaead99801
Wolfgang (Wolle) Ewald <wolfgang.ewald@wolles-elektronikkiste.de>
2020-12-23 16:05:35 +0100

    Add files via upload

---
+++ examples/ADXL345_basic_data/ADXL345_basic_data.ino
@@ -0,0 +1,99 @@
+/***************************************************************************
+* Example sketch for the ADXL345_WE library
+*
+* This sketch shows how to get the basic raw and g values 
+*  
+* Further information can be found on:
+* http://wolles-elektronikkiste.de
+* 
+***************************************************************************/
+
+#include<Wire.h>
+#include<ADXL345_WE.h>
+#define ADXL345_I2CADDR 0x53
+
+ADXL345_WE myAcc(ADXL345_I2CADDR);
+// ADXL345_WE myAcc = ADXL345_WE(); // Alternative: sets default address 0x53
+
+void setup(){
+  Wire.begin();
+  Serial.begin(9600);
+  Serial.println("ADXL345_Sketch - Basic Data");
+  Serial.println();
+  if(!myAcc.init()){
+    Serial.println("ADXL345 not connected!");
+  }
+   
+/* Choose the data rate         Hz
+    ADXL345_DATA_RATE_3200    3200
+    ADXL345_DATA_RATE_1600    1600
+    ADXL345_DATA_RATE_800      800
+    ADXL345_DATA_RATE_400      400
+    ADXL345_DATA_RATE_200      200
+    ADXL345_DATA_RATE_100      100
+    ADXL345_DATA_RATE_50        50
+    ADXL345_DATA_RATE_25        25
+    ADXL345_DATA_RATE_12_5      12.5  
+    ADXL345_DATA_RATE_6_25       6.25
+    ADXL345_DATA_RATE_3_13       3.13
+    ADXL345_DATA_RATE_1_56       1.56
+    ADXL345_DATA_RATE_0_78       0.78
+    ADXL345_DATA_RATE_0_39       0.39
+    ADXL345_DATA_RATE_0_20       0.20
+    ADXL345_DATA_RATE_0_10       0.10
+*/
+  myAcc.setDataRate(ADXL345_DATA_RATE_50);
+  Serial.print("Data rate: ");
+  Serial.print(myAcc.getDataRateAsString());
+
+/* In full resolution raw values the size of the raw values depend 
+    on the range: 2g = 10 bit; 4g = 11 bit; 8g = 12 bit; 16g =13 bit;
+    uncomment to change to 10 bit for all ranges. 
+ */
+  // myAcc.setFullRes(false);
+
+/* Choose the measurement range
+    ADXL345_RANGE_16G    16g     
+    ADXL345_RANGE_8G      8g     
+    ADXL345_RANGE_4G      4g   
+    ADXL345_RANGE_2G      2g
+*/ 
+  myAcc.setRange(ADXL345_RANGE_4G);
+  Serial.print("  /  g-Range: ");
+  Serial.println(myAcc.getRangeAsString());
+  Serial.println();
+
+/* Uncomment to enable Low Power Mode. It saves power but slightly
+    increases noise. LowPower only affetcs Data Rates 12.5 Hz to 400 Hz.
+*/
+  // myAcc.setLowPower(true);
+}
+
+/* The LSB of the Data registers is 3.9 mg (milli-g, not milligramm).
+    This value is used calculating g from raw. However, this is an ideal
+    value which you might want to calibrate. 
+*/
+
+void loop() {
+  xyzFloat raw = myAcc.getRawValues();
+  xyzFloat g = myAcc.getGValues();
+  
+  Serial.print("Raw-x = ");
+  Serial.print(raw.x);
+  Serial.print("  |  Raw-y = ");
+  Serial.print(raw.y);
+  Serial.print("  |  Raw-z = ");
+  Serial.println(raw.z);
+
+  Serial.print("g-x   = ");
+  Serial.print(g.x);
+  Serial.print("  |  g-y   = ");
+  Serial.print(g.y);
+  Serial.print("  |  g-z   = ");
+  Serial.println(g.z);
+
+  Serial.println();
+  
+  delay(1000);
+  
+}
