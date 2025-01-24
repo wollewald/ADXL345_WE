08eb5426c4375efa1b8c71526ace08ad2fe0c263
Wolfgang (Wolle) Ewald <wolfgang.ewald@wolles-elektronikkiste.de>
2025-01-24 18:31:59 +0100

    Add files via upload

--- examples/ADXL345_calibration/ADXL345_calibration.ino
+++ examples/ADXL345_calibration/ADXL345_calibration.ino
@@ -23,34 +23,35 @@ ADXL345_WE myAcc = ADXL345_WE(ADXL345_I2CADDR);
 
 void setup(){
   Wire.begin();
-  Serial.begin(9600);
-  Serial.println("ADXL345_Sketch - Calibration");
+  Serial.begin(115200);
+  Serial.println(F("ADXL345_Sketch - Calibration"));
   Serial.println();
   if(!myAcc.init()){
-    Serial.println("ADXL345 not connected!");
+    Serial.println(F("ADXL345 not connected!"));
   }
-  Serial.println("Calibration procedure:");
-  Serial.println(" - stay in full resolution");
-  Serial.println(" - supply voltage has influence (at least for the modules)");
-  Serial.println("        -> choose the same voltage you will use in your project!"); 
-  Serial.println(" - turn your ADXL slowly (!) to find the min and max raw x,y and z values");
-  Serial.println(" - deviations of one or two units don't matter much");
-  Serial.println(" - the calibration changes the slope of g vs raw and assumes zero is (min+max)/2 ");
-  Serial.println(" - write down the six values ");
-  Serial.println(" - you can try the calibration values in ADXL345_angles_tilt_orientation.ino example sketch");
-  Serial.println(" - ready to go? Then type in any key and send. ");
+  Serial.println(F("Calibration procedure:"));
+  Serial.println(F(" - stay in full resolution"));
+  Serial.println(F(" - supply voltage has influence (at least for the modules)"));
+  Serial.println(F("        -> choose the same voltage you will use in your project!")); 
+  Serial.println(F(" - turn your ADXL slowly (!) to find the min and max raw x,y and z values"));
+  Serial.println(F(" - deviations of one or two units don't matter much"));
+  Serial.println(F(" - the calibration changes the slope of g vs raw and assumes zero is (min+max)/2 "));
+  Serial.println(F(" - write down the six values "));
+  Serial.println(F(" - you can try the calibration values in ADXL345_angles_tilt_orientation.ino example sketch"));
+  Serial.println(F(" - ready to go? Then type in any key and send. "));
   while(!Serial.available());
   Serial.read();
   Serial.println(); Serial.println(); Serial.println();  
 }
 
 void loop() {
-  xyzFloat raw = myAcc.getRawValues();
-  Serial.print("Raw-x = ");
+  xyzFloat raw;
+  myAcc.getRawValues(&raw);
+  Serial.print(F("Raw-x = "));
   Serial.print(raw.x);
-  Serial.print("  |  Raw-y = ");
+  Serial.print(F("  |  Raw-y = "));
   Serial.print(raw.y);
-  Serial.print("  |  Raw-z = ");
+  Serial.print(F("  |  Raw-z = "));
   Serial.println(raw.z);
   
   delay(1000);
