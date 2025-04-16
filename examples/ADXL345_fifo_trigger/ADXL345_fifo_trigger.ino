f27c41e2ff160bebfd5cacc6a9e1b56937d3ef50
Wolfgang (Wolle) Ewald <wolfgang.ewald@wolles-elektronikkiste.de>
2025-04-16 14:26:48 +0200

    Update ADXL345_fifo_trigger.ino

--- examples/ADXL345_fifo_trigger/ADXL345_fifo_trigger.ino
+++ examples/ADXL345_fifo_trigger/ADXL345_fifo_trigger.ino
@@ -1,5 +1,5 @@
 /***************************************************************************
-* Example sketch for the ADXL345_WE library
+* Example sketch for the ADXL345_WE / ADXL343_WE library
 *
 * This sketch shows how to use the FIFO in trigger mode.
 *   
